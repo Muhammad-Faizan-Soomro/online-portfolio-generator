e937a46fa01720ab44d5a24bc5469ff9e0c79b24
Muhammad Faizan Soomro <mfaizansoomro00@gmail.com>
2023-12-31 00:09:19 +0500

    update layout.js

--- app/layout.js
+++ app/layout.js
@@ -29,7 +29,9 @@ export default function RootLayout({ children }) {
       ) : (
         <html lang="en">
           <body className="main-content body" suppressHydrationWarning={true}>
+          <AuthProvider>
             {children}
+          </AuthProvider>
           </body>
         </html>
       )}
