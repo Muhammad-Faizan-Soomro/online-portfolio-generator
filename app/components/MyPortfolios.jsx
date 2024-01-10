629a6663991d65bd1ed1bd33244d35dbd9ae0695
Muhammad Faizan Soomro <mfaizansoomro00@gmail.com>
2024-01-10 22:03:43 +0500

    Update MyPortfolios.jsx

--- app/components/MyPortfolios.jsx
+++ app/components/MyPortfolios.jsx
@@ -20,7 +20,7 @@ function MyPortfolios({ data }) {
        <div className="templates w-full h-full   flex flex-col gap-10  lag:gap-4 lg:px-10  lg:grid grid-cols-3 lg:mb-2  items-center justify-start pt-4">
         {data.map((datas) =>
           datas.user_id == userId ? (        
-                <div className="template8  group relative overflow-hidden border-2  dark:border-white  w-[350px] h-[230px]  flex flex-col">
+                <div key={datas._id] className="template8  group relative overflow-hidden border-2  dark:border-white  w-[350px] h-[230px]  flex flex-col">
                   <div className="img w-[100%] h-[100%] ">
                     <Image
                       src={
