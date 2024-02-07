ac62b883baee292c9caa83847761881a94a4cd33
Muhammad Faizan Soomro <mfaizansoomro00@gmail.com>
2024-02-08 02:01:39 +0500

    Update Button.jsx

--- app/components/Button.jsx
+++ app/components/Button.jsx
@@ -5,6 +5,7 @@ function Button({text, className,type="button"}) {
     <button
       className={`w-[25rem] m-4 py-3 text-white font-bold bg-blue-400 rounded-full hover:bg-purple-400 ${className}`}
       type={type}
+      aria-label={text}
     >
       {text}
     </button>
