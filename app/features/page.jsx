ad977459339335e8d8124f5995c66275e4edefde
Muhammad Faizan Soomro <mfaizansoomro00@gmail.com>
2024-02-08 01:53:46 +0500

    update UI

--- app/features/page.jsx
+++ app/features/page.jsx
@@ -9,19 +9,21 @@ function page() {
   return (
     <>
       <Navbar />
-      <h1 className="text-3xl  lg:text-5xl text-center font-bold text-orange-400 mt-4 dark:text-white ">
-        OUR FEATURES
+      <h1 className="text-3xl  lg:text-5xl text-center font-bold text-orange-400 my-8 lg:my-16 dark:text-white ">
+        WE PROVIDE
       </h1>
-      <div className="features   w-full h-full  flex flex-col lg:flex-row lg:justify-evenly items-center gap-10 mt-20 ">
-        <div className="features w-[300px] flex flex-col ">
+      <div className="features   w-full h-full  flex flex-col lg:flex-row lg:px-36 items-center gap-10 ">
+        <div className="features flex flex-col items-center justify-center lg:w-[45%] ">
           <Image
             src={f1}
             alt="f1.webp"
             priority
-            style={{ width: "300px", height: "auto" }}
+            style={{ width: "200px", height: "auto" }}
           />
-          <h1 className="text-center text-2xl font-bold">Easy To Use</h1>
-          <p className="text-justify font-bold text-blue-900 mt-2 dark:text-gray-300">
+          <h1 className="text-center text-2xl font-bold my-3">
+            User Friendly Interface
+          </h1>
+          <p className="text-left font-bold text-blue-900 lg:px-4 px-16 dark:text-gray-300 md:px-24">
             Creating your portfolio is a breeze with our user-friendly website
             for portfolio generation. Effortlessly showcase your work, customize
             designs with intuitive tools, and launch a professional portfolio in
@@ -29,38 +31,37 @@ function page() {
             hassle-free platform.
           </p>
         </div>
-        <div className="features w-[300px] h-full  flex flex-col  ">
+        <div className="features flex flex-col items-center justify-center lg:w-[40%] ">
           <Image
             src={f3}
             alt="f3.webp"
             priority
             style={{ width: "300px", height: "auto" }}
           />
-          <h1 className="text-center text-2xl font-bold ">
-            Predefined Templates
+          <h1 className="text-center text-2xl font-bold my-3">
+            Responsive Templates
           </h1>
-          <p className="text-justify font-bold text-blue-900 mt-2 dark:text-gray-300 ">
+          <p className="text-left font-bold text-blue-900 px-16 lg:px-4 dark:text-gray-300 md:px-24">
             Crafting your professional portfolio is a snap with our platform's
-            predefined templates. Choose from a variety of stylish designs,
-            easily personalize content, and launch your customized portfolio in
-            no time. Showcase your achievements in a polished and impressive way
-            without the hassle of starting from scratch.
+            pre-defined responsive templates. Choose from a variety of stylish
+            designs, easily personalize content, and launch your customized
+            portfolio in no time. Showcase your achievements in a polished and
+            impressive way without the hassle of starting from scratch.
           </p>
         </div>
-        <div className="features w-[350px] h-full  flex flex-col ">
+        <div className="features flex flex-col items-center justify-center lg:w-[30%]" >
           <Image
             src={f2}
             alt="f2.webp"
             priority
-            style={{ width: "350px", height: "auto" }}
+            style={{ width: "200px", height: "auto" }}
           />
-          <h1 className="text-center text-2xl font-bold">Responsive Design</h1>
-          <p className="text-justify font-bold text-blue-900 mt-2 dark:text-gray-300">
-            Our portfolio generator ensures a seamless and responsive design
-            experience. Effortlessly create a stunning portfolio that adapts to
-            any device be it a desktop, tablet, or smartphone. Showcase your
-            work beautifully while providing a consistently engaging experience
-            for your audience, regardless of the screen size.
+          <h1 className="text-center text-2xl font-bold my-3">
+            Saving Your Portfolio
+          </h1>
+          <p className="text-left font-bold text-blue-900 px-16 lg:px-4 dark:text-gray-300 mb-2 md:px-24">
+            Enhance your experience by downloading your personalized portfolio,
+            showcasing your best work in a convenient package.
           </p>
         </div>
       </div>
