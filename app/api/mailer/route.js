fe358a749b5599e6e72777b8d4c16d97aab93f1b
Muhammad Faizan Soomro <mfaizansoomro00@gmail.com>
2024-02-09 09:20:27 +0500

    update mail functionality

--- app/api/mailer/route.js
+++ app/api/mailer/route.js
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 
+
 export async function POST(req, res) {
   const response = await req.json();
   let nodemailer = require("nodemailer");
@@ -7,14 +8,14 @@ export async function POST(req, res) {
     port: 465,
     host: "smtp.gmail.com",
     auth: {
-      user: "mfaizansoomro00@gmail.com",
+      user: "onlineportfoliogenerator@gmail.com",
       pass: process.env.NEXT_PUBLIC_PASSWORD,
     },
     secure: true,
   });
 
   const mailData = {
-    from: "mfaizansoomro00@gmail.com",
+    from: "onlineportfoliogenerator@gmail.com",
     to: response.email,
     subject: `Steps To Host Your Portfolio On Github`,
     html: "<h1>Dear User</h1><br/><p>I hope this email finds you well. We're thrilled that you've decided to download and create your HTML portfolio! To help you showcase your work to the world, we've put together a step-by-step guide on how to host your portfolio online. Follow these instructions, and soon your creations will be accessible to a global audience.</p><br/><h1>Choose a Hosting Provider:</h1><br/><p>Select a reliable hosting provider to make your portfolio accessible on the internet. Popular options include GitHub Pages, Netlify, and Vercel. Each has its own advantages, so choose the one that best fits your needs.</p><br/><h1>Create an Account:</h1><p>Sign up for an account on the hosting platform of your choice. Follow the registration process and verify your email address.</p><br/><h1>Upload Your Portfolio:</h1><p>Once you're logged in, look for an option to upload or deploy your website. This may involve creating a new repository (for GitHub Pages) or connecting your repository (for Netlify and Vercel). Follow the platform-specific instructions for this step.</p>",
